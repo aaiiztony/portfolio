56522da8f882eca5eff46981731216fdf70d9f37
Aaiiztony <aaiiztony@gmail.com>
2023-04-09 16:14:19 +0530

    added navbar day01

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import Navbar from "./components/Navbar"
+import { BrowserRouter } from "react-router-dom"
+import {Navbar, About, Contact, Experience, Hero, Works, StarsCanvas} from "./components"
 
 const App =()=> {
   return (
-    <div className="App">
-      <Navbar/>
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat">
+        <Navbar/>
+        <Hero/>
+        <About/>
+        <Experience/>
+        <Works/>
+      </div>
+    <div className="relative z-0">
+      <Contact/>
+      <StarsCanvas/>
     </div>
+    </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 const Navbar = () => {
+  const [active, setActive] = useState("");
   return (
-    <div className='text-center bg-dark'>
-      Hi
-    </div>
-  )
-}
+    <nav
+      className={`${styles.paddingX} w-full flex items-center fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Tony
+            <span className="sm:block hidden">| Prasenjeet Ghosal</span>
+          </p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title 
+                ? "text-white" 
+                : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+            >
+              <a 
+              href={`#${link.id}`}>
+                {link.title}
+              </a>
+            </li>
+          ))}
+        </ul>
 
-export default Navbar
+        {/* use the menu and close here to form a div for the links due to responsiveness */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
